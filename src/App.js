3b2560d541710dc5bf86c268e9cdbc842e28bed6
enigm413 <rohithkkannikath@gmail.com>
2023-12-11 12:21:30 +0530

    Added For Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+const itemData = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 //Function to define Main App Component
 export default function App() {
   return (
     <div className="app">
       <Logo />
+      <Form />
       <Stats />
     </div>
   );
@@ -11,6 +17,22 @@ export default function App() {
 //Function to Define Logo Component
 function Logo() {
   return <h1>🌴🌴 Far Away 💼💼</h1>;
+}
+
+//Function to define Form Component
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for your 😍 Trip?</h3>
+      <select>
+        {[...Array(20).keys()].map((indexValue) => (
+          <option value={indexValue}>{indexValue + 1}</option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item....." />
+      <button>Add Item</button>
+    </form>
+  );
 }
 
 //Function to Define Stats Component
